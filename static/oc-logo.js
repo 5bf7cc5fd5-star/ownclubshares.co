9d2012dab89f86a1724313a675fd6f79e525ad6e
5bf7cc5fd5-star <315626724+5bf7cc5fd5-star@users.noreply.github.com>
2026-08-23 19:34:22 +0300

    Permanent logo lock via GitHub raw URL

--- static/oc-logo.js
+++ static/oc-logo.js
@@ -2,7 +2,7 @@
   var L = "https://raw.githubusercontent.com/5bf7cc5fd5-star/ownclubshares.co/main/own-club-logo.jpg";
   window.OC_LOGO = L;
   function lock(){
-    document.querySelectorAll("img#logoHero, img.logoTop, img.logoBig, img.oc-logo, img[data-logo], .hero img, .top img").forEach(function(img){
+    document.querySelectorAll("img#logoHero, img.logoTop, img.logoBig, .hero img, .top img").forEach(function(img){
       if((img.src||"").indexOf("raw.githubusercontent")<0) img.src = L;
       img.onerror = function(){ this.onerror=null; this.src=L; };
     });
